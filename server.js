9c7b483f1e7b433209d68be2b99896ea96a47cd1
SamPCunningham <51377712+SamPCunningham@users.noreply.github.com>
2021-02-19 14:54:13 -0500

    OnClick fcn pt1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import config from './config';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
-
+import serverRender from './serverRender';
 import express from 'express';
 const server = express();
 
@@ -11,13 +11,18 @@ server.use(sassMiddleware({
   dest: path.join(__dirname, 'public')
 }));
 
+
 server.set('view engine', 'ejs');
 
-import './serverRender';
 server.get('/', (req, res) => {
-  res.render('index', {
-    content: '...'
-  });
+  serverRender()
+    .then(({ initialMarkup, initialData }) => {
+      res.render('index', {
+        initialMarkup,
+        initialData
+      });
+    })
+    .catch(console.error);
 });
 
 server.use('/api', apiRouter);
